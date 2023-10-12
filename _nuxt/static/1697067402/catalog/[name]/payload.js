201d9a51c317f9223d201b6cc4fbcec7114dadd8
Obliviate <vyacheslavovna.an@mail.ru>
2023-10-12 08:10:31 +0600

    d3

--- _nuxt/static/1697067402/catalog/[name]/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/catalog/[name]", {data:[{}],fetch:{},mutations:void 0});
--- _nuxt/static/1697067402/catalog/[name]/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fis-tires\u002F_nuxt\u002Fstatic\u002F1697067402",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcatalog\u002F%5Bname%5D",config:{_app:{basePath:"\u002Fis-tires\u002F",assetsPath:"\u002Fis-tires\u002F_nuxt\u002F",cdnURL:a}}}}(null));
